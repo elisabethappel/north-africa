2d6c91ba1769482d301f217114726e1d0fefdbd8
Elisabeth Appel <73082739+elisabethappel@users.noreply.github.com>
2021-02-11 08:14:03 -0500

    condense capitals into listener re feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,12 @@
-$('.name').on('click', function() {
-  var flagUrl = $(this).attr('country-flag')
+$('.name').hover(function() {
+  var flagUrl = $(this).attr('data-country-flag')
   console.log(flagUrl)
   $('#flag').empty()
   $('#flag').css('background-image', `url(${flagUrl})`)
 })
 
-$('.name-2').on('click', function() {
-  var flagUrl = $(this).attr('country-flag')
-  console.log(flagUrl)
-  $('#flag').empty()
-
-  $('#flag').css('background-image', `url(${flagUrl})`)
-})
-
-
-$('#egypt').hover(function() {
-  $('.description').text('Cairo');
-}, function() {
-  $('.description').empty();
-});
-
-$('#tunisia').hover(function() {
-  $('.description').text('Tunis');
-}, function() {
-  $('.description').empty();
-});
-
-$('#morocco').hover(function() {
-  $('.description').text('Rabat');
-}, function() {
-  $('.description').empty();
-});
-
-$('#algeria').hover(function() {
-  $('.description').text('Algiers');
-}, function() {
-  $('.description').empty();
-});
-
-$('#libya').hover(function() {
-  $('.description').text('Tripoli');
-}, function() {
-  $('.description').empty();
-});
-
-$('#sudan').hover(function() {
-  $('.description').text('Khartoum');
-}, function() {
-  $('.description').empty();
-});
-
-$('#sadr').hover(function() {
-  $('.description').text('El Aaiún');
-}, function() {
-  $('.description').empty();
+$('.name').click(function() {
+  var capital = $(this).attr('data-capital')
+  console.log(capital)
+  $('.description').text(capital);
 });
